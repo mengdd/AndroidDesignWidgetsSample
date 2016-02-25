11c2282eb9944040c1a1eaee5d2c6a66623886ac
Dandan MENG <mengdandanno1@163.com>
2016-02-25 14:27:27 +0800

    add Snackbar

--- app/src/main/java/com/ddmeng/appbarlayoutsample/DetailsActivity.java
+++ app/src/main/java/com/ddmeng/appbarlayoutsample/DetailsActivity.java
@@ -2,11 +2,14 @@ package com.ddmeng.appbarlayoutsample;
 
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class DetailsActivity extends AppCompatActivity {
     @Bind(R.id.collapsing_toolbar)
@@ -25,4 +28,16 @@ public class DetailsActivity extends AppCompatActivity {
 
         collapsingToolbarLayout.setTitle("Details Title");
     }
+
+    @OnClick(R.id.floating_action_button)
+    void onFABClicked(View view) {
+        Snackbar.make(view, "Hi, I'm a Snackbar", Snackbar.LENGTH_LONG)
+                .setAction("Action", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // DO Sth when Action clicked
+                    }
+                }).show();
+    }
+
 }
