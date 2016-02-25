93bdc29efb24d486dd8cccbf32e33b7e419b17ea
Dandan MENG <mengdandanno1@163.com>
2016-02-25 11:56:51 +0800

    set parent activity and title for DetailsActivity

--- app/src/main/java/com/ddmeng/appbarlayoutsample/DetailsActivity.java
+++ app/src/main/java/com/ddmeng/appbarlayoutsample/DetailsActivity.java
@@ -1,13 +1,28 @@
 package com.ddmeng.appbarlayoutsample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class DetailsActivity extends AppCompatActivity {
+    @Bind(R.id.collapsing_toolbar)
+    CollapsingToolbarLayout collapsingToolbarLayout;
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        collapsingToolbarLayout.setTitle("Details Title");
     }
 }
